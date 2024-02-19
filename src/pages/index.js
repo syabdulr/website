b456fa895f247cc51b78a4abc129826b8abd4f03
Abdul Syed <80271158+syabdulr@users.noreply.github.com>
2024-02-19 11:13:36 -0500

    Update index.js
    fix typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
             </div>
             <div className='w-3/5 flex flex-col items-center self-center lg:w-full lg:text-center'>
               <AnimatedText
-                text="Elevating User Experiences through Emapathy."
+                text="Elevating User Experiences through Empathy."
                 className='!text-6xl !text-left xl:!text-5xl lg:!text-center lg:!text=6xl md:!text-5xl sm:!text-3xl'
               />
               <p className='my-4 text-base font-medium md:text-sm sm:text-xs'>
