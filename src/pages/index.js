f52a3546026c2e08d8f363296ecee27aa1f1b146
Abdul Syed <80271158+syabdulr@users.noreply.github.com>
2024-04-25 01:56:37 -0400

    remove text
    imagination trumps knowledge

--- src/pages/index.js
+++ src/pages/index.js
@@ -81,11 +81,6 @@ const Home = () => {
       <TransitionEffect />
       <main className="w-full mb-16 flex flex-col items-center justify-center dark:text-light">
         <Layout className="pt-16">
-          <AnimatedText
-            text="Imagination Trumps Knowledge!"
-            className="mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl"
-          />
-
           <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
             <div className="col-span-12">
               <FeaturedProject
